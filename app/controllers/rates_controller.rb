3c2a0429e7dd6f916a4bc34a99256e021fd089f6
高橋智也 <takahashitomoya@takahashitomoyanoMacBook-Pro.local>
2019-11-19 20:47:16 +0900

    update: 評価機能

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,2 +1,22 @@
 class RatesController < ApplicationController
+    def create
+        goal = Goal.find(params[:goal_id])
+        rate = goal.rates.new(rate_params)
+        rate.save
+        redirect_to user_goal_path(goal.user.id, goal.id)
+
+    end
+
+    def update
+        goal = Goal.find(params[:goal_id])
+        rate = Rate.find_by(user_id: current_user.id, goal_id: goal.id)
+        rate.update(rate_params)
+        redirect_to user_goal_path(goal.user.id, goal.id)
+    end
+
+
+    private
+    def rate_params
+      params.require(:rate).permit(:user_id, :goal_id, :quantity)
+    end
 end
