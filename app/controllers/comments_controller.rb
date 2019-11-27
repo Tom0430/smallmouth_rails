7216e9119239623f9d99629f4598b775e217ce86
Tom0430 <53653372+Tom0430@users.noreply.github.com>
2019-11-27 16:12:18 +0900

    add: エラーメッセージ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,17 @@
 class CommentsController < ApplicationController
     def create
-        goal = Goal.find(params[:goal_id])
+        @goal = Goal.find(params[:goal_id])
         comment = Comment.new(comment_params)
-        comment.goal_id = goal.id
+        comment.goal_id = @goal.id
         comment.user_id = current_user.id
-        if
-          comment.save
+        if comment.save
           redirect_to user_goal_path(comment.goal.user, comment.goal)
         else
-          render 'goals/show'
+        @comment_error_catcher = comment
+        @progress = Progress.new
+        @comment = Comment.new
+        @rate = Rate.find_or_initialize_by(user_id: current_user.id, goal_id: @goal.id)
+        render 'goals/show'
         end
     end
     def destroy
--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -1,10 +1,17 @@
 class ProgressesController < ApplicationController
     def create
-      goal = Goal.find(params[:goal_id])
-      progress = goal.progresses.new(progress_params)
-      progress.goal_id = goal.id
-      progress.save
-      redirect_to user_goal_path(goal.user.id, goal.id)
+      @goal = Goal.find(params[:goal_id])
+      progress = @goal.progresses.new(progress_params)
+      progress.goal_id = @goal.id
+      if progress.save
+        redirect_to user_goal_path(goal.user.id, goal.id)
+      else
+        @progress_error_catcher = progress
+        @progress = progress
+        @comment = Comment.new
+        @rate = Rate.find_or_initialize_by(user_id: current_user.id, goal_id: @goal.id)
+        render 'goals/show'
+      end
     end
     def destroy
       progress = Progress.find(params[:id])
