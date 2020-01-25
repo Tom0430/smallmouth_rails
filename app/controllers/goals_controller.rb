dae9861ccafc60591702da9c18a42d5098797228
tomoya takahashi <tiger@tomoyanoMacBook-Pro.local>
2020-01-26 00:40:27 +0900

    fix:チャレンジのデフォルト公開、非公開変更機能

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -8,6 +8,12 @@ class GoalsController < ApplicationController
     def create
         goal = Goal.new(goal_params)
         goal.user_id = current_user.id
+        binding.pry
+        if goal.user.default_published == true
+            goal.published = true
+        else
+            goal.published = false
+        end
         if goal.save
             redirect_to user_goal_path(goal.user.id, goal.id)
         else
