4de86e1eea99454589e5fb5864f4af56226d0e68
tomoya takahashi <qwdvgy03@gmail.com>
2020-03-21 18:02:56 +0900

    goalモデルのテスト修正、limit_timeをランダムに

--- spec/factories/goals.rb
+++ spec/factories/goals.rb
@@ -1,27 +1,28 @@
 FactoryBot.define do
 # to_aメソッドを使うと配列になって返ってくる
 # [0,1,2,3,4]をシャッフルして一番目の数字をlimit_time_numberに入れている
-# limit_time_number = (0..4).to_a.shuffle[0]
+    limit_time_number = (0..4).to_a.shuffle[0]
 
-# case limit_time_number
-# when 0
-#     limit_time = "limit1m"
-# when 1
-#     limit_time = "limit8h"
-# when 2
-#     limit_time = "limit1d"
-# when 3
-#     limit_time = "limit3d"
-# when 4
-#     limit_time = "limit1w"
-# end
-# return limit_time
+    case limit_time_number
+    when 0
+        limit_time = "limit1m"
+    when 1
+        limit_time = "limit8h"
+    when 2
+        limit_time = "limit1d"
+    when 3
+        limit_time = "limit3d"
+    when 4
+        limit_time = "limit1w"
+    end
+# return limit_time と記述するとエラーが起きる
+# rubyはreturnいらない、処理を止めたいならbreak
 
     factory :goal do
         user_id { 1 }
         title { "チャレンジ" }
         detail { "チャレンジ詳細" }
-        limit_time { "limit1w" }
+        limit_time { limit_time }
         published { true }
         association :user
     end
